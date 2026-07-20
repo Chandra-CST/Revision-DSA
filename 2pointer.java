9b1a0d80e5122abb102f433279ed7d25914c18f8
Chandra-CST <thakur.shubh2004@gmail.com>
2026-07-20 15:32:10 +0530

    Add two pointer pattern examples in 2pointer.java
    Implemented two pointer pattern for reversing an array and checking for palindromes.

---
+++ 2pointer.java
@@ -0,0 +1,40 @@
+
+Reversing an array | Two pointer pattern:
+class main{
+    public static void main(String [] args){
+        int [] arr = {1, 2, 3, 4, 5, 6};
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+
+    }
+}
+Palidrome:
+import java.util.Scanner;
+class main{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your data to check if it is palindrome or not: ");
+        
+        String s = sc.next();
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while(left < right){
+        if(s.charAt(left) != s.charAt(right)){
+            break;
+            }
+        }
+    }
+}
