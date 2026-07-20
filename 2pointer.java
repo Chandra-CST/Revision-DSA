2a4cb8a7bcec0efbccccfe9b0f9c1b521dcf3778
Chandra-CST <thakur.shubh2004@gmail.com>
2026-07-20 19:37:25 +0530

    Improve palindrome checking logic
    Refactor palindrome check to use a boolean flag for clarity.

--- 2pointer.java
+++ 2pointer.java
@@ -1,5 +1,5 @@
 
-Reversing an array | Two pointer pattern:
+// Reversing an array | Two pointer pattern:
 class main{
     public static void main(String [] args){
         int [] arr = {1, 2, 3, 4, 5, 6};
@@ -19,7 +19,7 @@ class main{
 
     }
 }
-Palidrome:
+// Palidrome:
 import java.util.Scanner;
 class main{
     public static void main(String [] args){
@@ -30,11 +30,21 @@ class main{
         String s = sc.next();
         int left = 0;
         int right = s.length() - 1;
-        
-        while(left < right){
-        if(s.charAt(left) != s.charAt(right)){
-            break;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                isPalindrome = false;
+                break;
             }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.print("Palindrome");
+        } else {
+            System.out.print("Not Palindrome");
         }
     }
 }
