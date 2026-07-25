be08070e942a3f887e6c4980dfe47126bf13869e
Chandra-CST <thakur.shubh2004@gmail.com>
2026-07-25 15:06:30 +0530

    Implement prime number counting in Mathematics.java
    Mathematical operations such as armstrong number,Palindrome,Prime Numbers,ttraversal as well as pattern recognision

--- Mathematics.java
+++ Mathematics.java
@@ -120,3 +120,43 @@ class main{
         }
     }
 }
+
+Prime number and printing the divisors: 
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a value: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        for(int i = 1; i <= n; i++) {
+
+            boolean isPrime = true;
+
+            if(i <= 1) {
+                isPrime = false;
+            }
+            else {
+                for(int j = 2; j <= Math.sqrt(i); j++) {
+
+                    if(i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if(isPrime) {
+                count++;
+            }
+        }
+
+        System.out.println("Number of primes: " + count);
+    }
+}
