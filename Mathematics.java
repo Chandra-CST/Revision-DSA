97c898bd48211a939fa991ee972e7797b4e37c5e
Chandra-CST <thakur.shubh2004@gmail.com>
2026-07-25 15:00:56 +0530

    Add palindrome checking functionality in Mathematics.java

--- Mathematics.java
+++ Mathematics.java
@@ -14,3 +14,24 @@ class main{
         System.out.print("Digit count: " + count);
     }
 }
+// Palindrome:
+import java.util.Scanner;
+class main{
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value: ");
+        int num = sc.nextInt();
+        int original = num;
+        int reverse = 0;
+
+        while(num > 0){
+            int digit = num % 10;                     (% helps in extracting the digits, simply put i gives us a remainder which is the last di)
+            reverse = reverse * 10 + digit;
+            num = num/10;
+        }
+        if(reverse == original)
+            System.out.print("Palindrome");
+            else{
+            System.out.print("Not Palindrome");
+            }
+        }
