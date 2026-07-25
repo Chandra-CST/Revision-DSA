8387621dbc195a846de83fd8d80c7e73c7c069c7
Chandra-CST <thakur.shubh2004@gmail.com>
2026-07-25 15:01:48 +0530

    Implement Armstrong number check in Mathematics.java
    Added functionality to check if a number is an Armstrong number.

--- Mathematics.java
+++ Mathematics.java
@@ -35,3 +35,47 @@ class main{
             System.out.print("Not Palindrome");
             }
         }
+//   Armstrong Number:
+import java.util.Scanner;
+class main{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value: ");
+        int n = sc.nextInt();
+
+        int original = n;
+
+        int count = 0;
+        int temp = n;
+
+        // First Traversal:
+        while(temp > 0){
+            count++;
+            temp = temp / 10;
+        }
+        // Edge case for 0:
+        if(n == 0){
+            count = 1;
+        }
+        // Second Traversal;
+        int sum = 0;
+        int temp = n;
+
+        while(temp > 0){
+            int digit = temp % 10;
+            sum += (int) Math.pow(digit, count);
+            temp = temp % 10;
+        }
+        // Edge case for 0 :
+        if(temp == 0){
+            count = 1;
+        }
+        // Comparing values tpo get an output:
+        if(sum == original){
+            System.out.print("Armstrong Number");
+        }
+        else{
+            System.out.print("Not an Armstrong Number");
+        }
+        }
+    }
