e5fac76451174d5d56836e01c4abf80bffee0361
Chandra-CST <thakur.shubh2004@gmail.com>
2026-07-28 23:46:24 +0530

    Add Fibonacci sequence example in Main class

--- fibo.java
+++ fibo.java
@@ -21,3 +21,27 @@ class Main {
         }
     }
 }
+
+// EXAMPLE-1:
+
+class Main {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        int prev = 0;
+        int curr = 1;
+
+        System.out.print(prev + " ");
+        System.out.print(curr + " ");
+
+        for(int i = 3; i <= n; i++) {
+            int next = prev + curr;
+
+            System.out.print(next + " ");
+
+            prev = curr;
+            curr = next;
+        }
+    }
+}
