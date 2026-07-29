d96e7ae78d35ae19f0182ba92b84c0e9415d47d3
Chandra Shekhar <thakur.shubh2004@gmail.com>
2026-07-30 00:15:31 +0530

    Added min element in an array

---
+++ Arrays/03_MinElement.java
@@ -0,0 +1,15 @@
+class MinElement{
+    public static void main(String [] args){
+
+        int [] arr = {1,5,61,32,2};
+
+        int min = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if( arr[i] == min){
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
