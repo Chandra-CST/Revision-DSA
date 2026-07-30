17d8aec7afefa41a87b14d0c4a0a07e0fedf452a
Chandra Shekhar <thakur.shubh2004@gmail.com>
2026-07-30 23:31:38 +0530

    Palindrome array

---
+++ Arrays/07_PalindromeArray.java
@@ -0,0 +1,25 @@
+class PalindromeArray{
+    public static void main(String [] args){
+
+        int[] arr = {1, 2, 2, 1};
+        int left = 0;
+        int right = arr.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The array is Palindrome");
+        } else {
+            System.out.println("The array is not Palindrome");
+        }
+    }
+}
