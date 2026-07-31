e3e7f167e703deb20659dc4d99bb19c96194c0c5
Chandra Shekhar <thakur.shubh2004@gmail.com>
2026-07-31 20:54:16 +0530

    Largest and Second Largest element in an array

--- Arrays/08_SecLargeElement.java
+++ Arrays/08_SecLargeElement.java
@@ -1,5 +1,23 @@
-class SecLargeElemennt{
+class SecLargeElement{
     public static void main(String [] args){
-        
+        int [] arr = {2,4,54,6,17,8,16};
+
+        int largest = arr[0];
+        int SecondLargest = arr[0];
+        int current;
+
+        for(int i = 1; i < arr.length; i++){
+            current = arr[i];
+
+         if(current > largest ){
+            int temp = largest;
+            largest = current;
+            SecondLargest = temp;
+            }
+            else if(SecondLargest < current && current < largest){
+                SecondLargest = current;
+            }
+        }
     }
 }
+    
