0e0e29ac36f38287fdbf4b1ce1908dcd2d37b55b
Chandra Shekhar <thakur.shubh2004@gmail.com>
2026-08-03 01:08:44 +0530

    Finding the last odd element

---
+++ Arrays/21_LastOdd.java
@@ -0,0 +1,16 @@
+class LastOdd{
+    public static void main(String [] args){
+
+        int[] arr = {10, 18, 5, 12, 9, 4, 7};
+
+        int LastOdd = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 != 0){
+                LastOdd = arr[i];
+            }
+        }
+        System.out.println(LastOdd);
+    }
+
+}
