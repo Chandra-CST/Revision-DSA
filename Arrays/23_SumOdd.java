14837b063349552acddfb17d6a0a8947dfc574dd
Chandra Shekhar <thakur.shubh2004@gmail.com>
2026-08-03 01:21:23 +0530

    Sum of odd element

---
+++ Arrays/23_SumOdd.java
@@ -0,0 +1,15 @@
+class SumOdd{
+    public static void main(String [] args){
+
+        int[] arr = {11, 8, 5, 12, 9, 4, 7};
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 != 0){
+                sum += arr[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
