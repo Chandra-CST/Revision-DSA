9b80e9cabbd4bcf7186cecd7d66d7685153a8270
Chandra Shekhar <thakur.shubh2004@gmail.com>
2026-08-02 23:09:46 +0530

    Sum of all positive integers inside an array

---
+++ Arrays/13_SumOfPos.java
@@ -0,0 +1,15 @@
+class SumOfPos{
+    public static void main(String [] args){
+
+        int[] arr = {5, -3, 0, 7, -1, 0, 9, -6};
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > 0){
+                sum++;
+            }
+        }
+        System.out.println("The sum of all Positive Numbers are : " + sum);
+    }
+}
