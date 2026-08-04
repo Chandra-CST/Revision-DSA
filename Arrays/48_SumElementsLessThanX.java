e003edf8fd01e896dbabdb68a613e7e1d911beb8
Chandra Shekhar <thakur.shubh2004@gmail.com>
2026-08-04 21:20:42 +0530

    sum of elements less than x

---
+++ Arrays/48_SumElementsLessThanX.java
@@ -0,0 +1,16 @@
+class SumElementsLessThanX{
+    public static void main(String [] args){
+
+        int[] arr = {18, 7, 25, 10, 3, 21, 15};
+        int x = 15;
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < x){
+                sum += arr[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
