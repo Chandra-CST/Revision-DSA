9668734dc6766fcf0d1f605493de27dab1d7f007
Chandra Shekhar <thakur.shubh2004@gmail.com>
2026-08-04 19:10:52 +0530

    Sum of multiples of 5

---
+++ Arrays/38_SumMultiplesOf5.java
@@ -0,0 +1,19 @@
+class SumOfMultiplesOf5{
+    public static void main(String [] args){
+
+        int[] arr = {10, 7, 15, 22, 30, 9, 5};
+
+        int count = 0;
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 5 == 0){
+                count++;
+                sum += arr[i];
+            }
+        }
+        System.out.println(count);
+        
+        System.out.println(sum);
+    }
+}
