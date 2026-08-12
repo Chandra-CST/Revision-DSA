ecde362ad3aad280ffb3ff3f00323e4fa00585d7
Chandra Shekhar <175748970+Chandra-CST@users.noreply.github.com>
2026-08-13 00:27:38 +0530

    Put zeroes at the end

---
+++ Arrays/81_PutZeroesAtTheEnd.java
@@ -0,0 +1,22 @@
+class PutZeroesAtTheEnd{
+    public static void main(String [] args){
+
+        int[] arr = {0, 1, 0, 3, 12};
+
+        int j = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0){
+                arr[j] = arr[i];
+                j++;                
+            }
+        }
+          for(int i = j; i < arr.length; i++){
+            arr[i] = 0;
+        }
+
+        for(int k = 0; k < arr.length; k++){
+            System.out.print(arr[k] + " ");
+            }
+    }
+}
