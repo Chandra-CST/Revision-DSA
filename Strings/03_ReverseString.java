7844331bf39049ce713293cd59b93c1363ed18f3
Chandra Shekhar <175748970+Chandra-CST@users.noreply.github.com>
2026-08-25 00:56:45 +0530

    Reverse a String

---
+++ Strings/03_ReverseString.java
@@ -0,0 +1,22 @@
+class ReverseString {
+    public static void main(String[] args) {
+
+        String s = "hello";
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right) {
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.println(new String(arr));
+    }
+}
