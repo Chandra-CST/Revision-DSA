b570e2e025816e502ed4b0a3845975a41fcdbed9
Chandra Shekhar <175748970+Chandra-CST@users.noreply.github.com>
2026-09-09 13:26:28 +0530

    First Recursive program

---
+++ Recursion/Basics.java
@@ -0,0 +1,14 @@
+class Recursion {
+
+    static void printNumbers(int num){
+        if(num == 0){
+            return;
+        }
+        System.out.println(num);
+        printNumbers(num - 1);
+    }
+    public static void main(String[] args){
+       printNumbers(5);
+    }
+
+}
