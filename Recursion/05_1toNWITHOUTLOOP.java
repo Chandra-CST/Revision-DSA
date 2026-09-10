43a6a131924b13235b21c60ddfe15df5a4bdbe55
Chandra Shekhar <175748970+Chandra-CST@users.noreply.github.com>
2026-09-10 17:51:07 +0530

    Printing 1 to N and vice versa without using a loop

---
+++ Recursion/05_1toNWITHOUTLOOP.java
@@ -0,0 +1,16 @@
+class OneToNWithoutLoop {
+
+    static void OnetoN(int n) {
+        if(n == 0){
+            return;
+        }
+        OnetoN(n - 1);
+        System.out.println(n);
+    }
+
+
+    public static void main(String[] args){
+        OnetoN(5);
+    }
+
+}
