6520fea3b1d70b90159bb7e5b671d6efb1e4a9eb
Chandra Shekhar <175748970+Chandra-CST@users.noreply.github.com>
2026-09-10 16:33:22 +0530

    Understanding loops and palindrome

--- Recursion/04_RecursiveArray.java
+++ Recursion/04_RecursiveArray.java
@@ -1,5 +1,30 @@
 class Recursion{
 
+    static boolean isPalindrome(String str, int left, int right){
+
+        if(left >= right){
+            return true;
+        }
+        if( str.charAt(left) != str.charAt(right) ){
+            return false;
+        }
+        return isPalindrome(str, left +1, right - 1);
+
+    }
+
+    static int[] arr = {8, 12, 20, 25};
+    static boolean searching(int[] arr, int index, int target){
+
+        if( index == arr.length ){
+            return false;
+        }
+        if(arr[index] == target){
+            return true;
+        }
+        return searching(arr, index + 1, target);
+
+    }
+
     static boolean search(int[] arr, int index, int target){
 
         if(index == arr.length){
@@ -12,7 +37,9 @@ class Recursion{
 
     }
     public static void main(String[] args){
-        int[] arr = {5,10,15,20};
-        System.out.println(search(arr,0,23));
+        // int[] arr = {5,10,15,20};
+        // System.out.println(search(arr,0,23));
+        // System.out.println(searching(arr,0,20));
+        System.out.println(isPalindrome("listen", 0, 4));
     }
 }
