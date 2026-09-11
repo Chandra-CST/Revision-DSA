cef41a9160c9b222682df4318399eab17200ace7
Chandra Shekhar <175748970+Chandra-CST@users.noreply.github.com>
2026-09-11 15:59:13 +0530

    Printing form one to N, counting digits, sum of digits using recursion

--- Recursion/05_1toNWITHOUTLOOP.java
+++ Recursion/05_1toNWITHOUTLOOP.java
@@ -1,5 +1,19 @@
 class OneToNWithoutLoop {
 
+    static int countDigits(int n){
+        if(n == 0){
+            return 0;
+        }
+        return 1 + countDigits(n / 10);
+    }
+
+    static int sumDigits(int n){
+        if(n == 0){
+            return 0;
+        }
+        return (n % 10) + sumDigits(n / 10);
+    }
+
     static void OnetoN(int n) {
         if(n == 0){
             return;
@@ -10,7 +24,10 @@ class OneToNWithoutLoop {
 
 
     public static void main(String[] args){
-        OnetoN(5);
+        // OnetoN(5);
+        int n = 12345;
+    //    System.out.print(sumDigits(n));
+       System.out.print(countDigits(n));
     }
 
 }
