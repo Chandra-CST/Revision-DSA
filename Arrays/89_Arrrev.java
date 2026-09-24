47825663e188e8a52d0dcc89534649f78c7ddd18
Chandra Shekhar <175748970+Chandra-CST@users.noreply.github.com>
2026-09-24 18:33:48 +0530

    Revising Arrays

--- Arrays/89_Arrrev.java
+++ Arrays/89_Arrrev.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 class Arrrrev {
 
+    // 1. Find Largest Element
+    static int largest(int[] arr) {
+
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        return largest;
+    }
+
+    // 2. Find Second Largest Element
     static int secondLargest(int[] arr) {
 
         int largest = arr[0];
@@ -19,70 +36,83 @@ class Arrrrev {
         return secondLargest;
     }
 
-    static int largest(int[] arr) {
-        int largest = arr[0];
+    // 3. Linear Search
+    static int linearSearch(int[] arr, int target) {
 
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > largest) {
-                largest = arr[i];
-            }
-        }
+        for (int i = 0; i < arr.length; i++) {
 
-        return largest;
-    }
-
-    static int LinearSearch(int [] arr, int target){
-
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == target){
+            if (arr[i] == target) {
                 return i;
             }
         }
 
         return -1;
     }
-    
-    static int binarySearch(int [] arr, int target){
+
+    // 4. Binary Search
+    static int binarySearch(int[] arr, int target) {
 
         int left = 0;
         int right = arr.length - 1;
 
-        while(left <= right){
+        while (left <= right) {
+
             int mid = left + (right - left) / 2;
 
-            if (arr[mid] == target){
+            if (arr[mid] == target) {
                 return mid;
             }
 
             if (arr[mid] < target) {
                 left = mid + 1;
             }
-            else if (arr[mid] > target) {
-                right = mid - 1;
-            }
             else {
-                return mid;
+                right = mid - 1;
             }
         }
 
-        return - 1;
-    } 
+        return -1;
+    }
+
+    // 5. Bubble Sort
+    static void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
-        int[] arr = {3, 8, 12, 17, 25, 31, 44};
+        // Largest
+        int[] arr1 = {7, 2, 9, 4, 1};
+        System.out.println("Largest: " + largest(arr1));
 
-        System.out.println(binarySearch(arr, 25));
+        // Second Largest
+        System.out.println("Second Largest: " + secondLargest(arr1));
 
+        // Linear Search
+        int[] arr2 = {10, 25, 7, 42, 19};
+        System.out.println("Linear Search: " + linearSearch(arr2, 42));
 
-        // int[] arr = {10, 25, 7, 42, 19};
+        // Binary Search
+        int[] arr3 = {3, 8, 12, 17, 25, 31, 44};
+        System.out.println("Binary Search: " + binarySearch(arr3, 25));
 
-        // int target = 42;
+        // Bubble Sort
+        int[] arr4 = {5, 3, 8, 1, 2};
 
-        // System.out.println(LinearSearch(arr, target));
+        bubbleSort(arr4);
 
-        // int[] arr = {10, 20, 15, 8, 25};
-
-        // System.out.println(secondLargest(arr));
-        // System.out.println(largest(arr));
+        System.out.println("Bubble Sort: " + Arrays.toString(arr4));
     }
 }
