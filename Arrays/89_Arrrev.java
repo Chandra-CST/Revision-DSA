3d62f2141895e845165fbe41aa9c5d42309a991e
Chandra Shekhar <175748970+Chandra-CST@users.noreply.github.com>
2026-09-24 17:43:31 +0530

    Revising Arrays

--- Arrays/89_Arrrev.java
+++ Arrays/89_Arrrev.java
@@ -1,17 +1,57 @@
-class Arrrrev{
+class Arrrrev {
 
-    // Given an integer array, return the largest element
-    public static void main(String [] args){
-        
-        int [] arr = {7, 2, 9, 4, 1};
+    static int secondLargest(int[] arr) {
 
-        int max = arr[0];
+        int largest = arr[0];
+        int secondLargest = Integer.MIN_VALUE;
 
-        for(int i = 0; i < arr.length; i++){
-            if( arr[i] > max){
-                max = arr[i];
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
             }
         }
-        System.out.println(max);
+
+        return secondLargest;
+    }
+
+    static int largest(int[] arr) {
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        return largest;
+    }
+
+    static int LinearSearch(int [] arr, int target){
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 42, 19};
+
+        int target = 42;
+
+        System.out.println(LinearSearch(arr, target));
+
+        // int[] arr = {10, 20, 15, 8, 25};
+
+        // System.out.println(secondLargest(arr));
+        // System.out.println(largest(arr));
     }
 }
